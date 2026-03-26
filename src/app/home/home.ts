50d69c963508ad238a457c2eb90e5f9672775363
Katie Loesch <katie.loesch@aveva.com>
2026-03-26 12:27:45 +0000

    add text + images to home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bot-home',
   imports: [],
-  template: '<p class="red">Inline Home</p>',
-  styles: [`.red {
-    color: darkred;
-    }`],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
 })
 export class Home {}
