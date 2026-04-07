811675700ac4bdfabfe71813ca465fdf8a4cd832
Katie Loesch <katie.loesch@aveva.com>
2026-04-07 15:46:17 +0100

    formatting data with angular pipes

--- src/app/catalog/catalog.ts
+++ src/app/catalog/catalog.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { NgFor, NgIf } from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'bot-catalog',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, CurrencyPipe],
   templateUrl: './catalog.html',
   styleUrl: './catalog.css',
 })
