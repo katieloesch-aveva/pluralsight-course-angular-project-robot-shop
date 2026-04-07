6b8119ee803f3df3749c4f0f21cb9c0d4d31553e
Katie Loesch <katie.loesch@aveva.com>
2026-04-07 10:48:09 +0100

    binding to component data with interpolation + using attribute bindings and functions

--- src/app/catalog/catalog.ts
+++ src/app/catalog/catalog.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './catalog.html',
   styleUrl: './catalog.css',
 })
-export class Catalog {}
+export class Catalog {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      description: 'A friendly robot head with 2 eyes and a smile -- great for domestic use.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+
+  getImgUrl(product: IProduct) {
+    return '/assets/images/robot-parts/' + product.imageName
+  }
+}
+
+
+
